39388a56e301c5047b74789c03bd1910d2b608a0
chenjian <chenjian097@qq.com>
2019-02-27 13:51:48 +0800

    增加头像

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,23 @@
+#!/usr/bin/env bash
+
+commit_message=
+while getopts ':m:' opt
+do
+    case $opt in
+        m)
+        commit_message=$OPTARG
+        ;;
+        ?)
+        echo "未知参数"
+        exit 1;;
+    esac
+done
+
 hexo clean
 hexo g
 hexo d
+
+echo ${commit_message}
 git add .
-git commit -m "hexo deploy"
+git commit -m "${commit_message}"
 git push origin hexo
